882ade70488cdefcfa66b231b5c86e70074125eb
FCsab <161705557+FCsab@users.noreply.github.com>
2024-08-05 23:16:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,20 +13,17 @@ function install_steam(){
      if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm;
  then
-      echo "Error installing RPMFusion repositories."
-      echo "Steam" >> failed.txt
+      echo "Downloading steam repo" >> failed.txt
       return 1
     fi
 
     if ! sudo dnf config-manager --set-enabled fedora-cisco-openh264 -y; then
-      echo "Error enabling fedora-cisco-openh264 repository."
-      echo "Steam" >> failed.txt
+      echo "Enabling steam repo" >> failed.txt
       return 1
     fi
 
     if ! sudo dnf install -y steam; then
-      echo "Error installing Steam."
-      echo "Steam" >> failed.txt
+      echo "Installing Steam" >> failed.txt
       return 1
     fi
     echo "Steam installation successful!"
@@ -35,6 +32,7 @@ function install_steam(){
 
 function post_install_setup(){
      if [[ "$system" == "Fedora" ]]; then
+     sudo cp /etc/dnf/dnf.conf /etc/dnf/dnf.conf.bak
         if ! sudo echo "[main]
 gpgcheck=1
 installonly_limit=3
@@ -45,8 +43,8 @@ fastestmirror=1
 max_parallel_downloads=10
 deltarpm=true" > /etc/dnf/dnf.conf
 then
-    echo "Error during setting up dnf."
     echo "DNF config" >> failed.txt
+    sudo cp /etc/dnf/dnf.conf.bak /etc/dnf/dnf.conf
     return 1
 fi
 }
@@ -55,7 +53,6 @@ function enable_bluetooth(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo systemctl enable --now bluetooth
             then
-                echo "Error during starting Bluetooth."
                 echo "Enabling bluetooth" >> failed.txt
                 return 1
         fi
@@ -67,31 +64,26 @@ function install_codecs(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf swap 'ffmpeg-free' 'ffmpeg' --allowerasing -y
             then
-                echo "Error during installing codecs."
                 echo "codecs" >> failed.txt
                 return 1
         fi
         if ! sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
             then
-                echo "Error during installing codecs."
                 echo "codecs" >> failed.txt
                 return 1
         fi
         if ! sudo dnf install ffmpeg ffmpeg-libs libva libva-utils -y
             then
-                echo "Error during installing codecs."
                 echo "codecs" >> failed.txt
                 return 1
         fi
         if ! sudo dnf config-manager --set-enabled fedora-cisco-openh264 -y
             then
-                echo "Error during enabling repository."
                 echo "codecs" >> failed.txt
                 return 1
         fi
         if ! sudo dnf install -y openh264 gstreamer1-plugin-openh264 mozilla-openh264 -y
             then
-                echo "Error during installing codecs."
                 echo "codecs" >> failed.txt
                 return 1
         fi
@@ -104,25 +96,21 @@ function install_protonge(){
         mkdir ~/.steam/root/compatibilitytools.d
             if ! wget -P ~/Downloads https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-11/GE-Proton9-11.tar.gz
                 then
-                    echo "Error during downloading ProtonGE."
                     echo "Installing ProtonGe" >> failed.txt
                     return 1
             fi
             if ! mkdir ~/.steam/root/compatibilitytools.d/
                 then
-                    echo "Error during creating directory."
                     echo "ProtonGe folder creation" >> failed.txt
                     return 1
             fi
             if ! tar -xf GE-Proton9-11.tar.gz -C ~/.steam/root/compatibilitytools.d/
                 then
-                    echo "Error during unpacking package."
                     echo "ProtonGe extract" >> failed.txt
                     return 1
             fi
             if ! rm ~/Downloads/GE-Proton9-11.tar.gz
                 then
-                    echo "Error during cleaning up."
                     echo "ProtonGe clean" >> failed.txt
                     return 1
             fi
@@ -133,13 +121,11 @@ function install_git(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install git -y
             then
-                echo "Error during installing Git."
                 echo "Installing git" >> failed.txt
                 return 1
         fi
         if ! flatpak install flathub io.github.shiftey.Desktop
             then
-                echo "Error during installing GitHub Desktop."
                 echo "Installing GitHub Desktop" >> failed.txt
                 return 1
         echo "Git installation successful!"
@@ -150,19 +136,16 @@ function install_discord(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
             then
-                echo "Error adding repo."
                 echo "Adding Discord repo" >> failed.txt
                 return 1
         fi
         if ! sudo dnf update -y
             then
-                echo "Error during updating."
                 echo "Updating for Discord" >> failed.txt
                 return 1
         fi
         if ! sudo dnf install discord -y
             then
-                echo "Error during installing Discord."
                 echo "Installing Discord" >> failed.txt
                 return 1
         fi
@@ -174,7 +157,6 @@ function install_grubcostum(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install grub-customizer -y
             then
-                echo "Error during installing Grub Customizer."
                 echo "Installing Grub Customizer" >> failed.txt
                 return 1
         fi
@@ -186,7 +168,6 @@ function install_gimp(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install gimp -y
             then
-                echo "Error during installing GIMP."
                 echo "Installing GIMP" >> failed.txt
                 return 1
         fi
@@ -198,13 +179,11 @@ function install_fish(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install fish -y
             then
-                echo "Error during installing Fish."
                 echo "Installing Fish" >> failed.txt
                 return 1
         fi
         if ! chsh -s /usr/bin/fish
             then
-                echo "Error during changing shell."
                 echo "Changing shell to Fish" >> failed.txt
                 return 1
         fi
@@ -216,19 +195,16 @@ function install_vscode(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             then
-                echo "Error during importing key."
                 return 1
                 echo "Importing key for Visual Studio Code" >> failed.txt
         fi
         if ! sudo dnf check-update
             then
-                echo "Error during checking for updates."
                 echo "Checking for updates for Visual Studio Code" >> failed.txt
                 return 1
         fi
         if ! sudo dnf install code -y
             then
-                echo "Error during installing Visual Studio Code."
                 echo "Installing Visual Studio Code" >> failed.txt
                 return 1
         fi
@@ -240,7 +216,6 @@ function install_rust(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install rust -y
             then
-                echo "Error during installing Rust."
                 echo "Installing Rust" >> failed.txt
                 return 1
         fi
@@ -252,13 +227,11 @@ function set_up_flatpak(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install flatpak -y
             then
-                echo "Error during installing Flatpak."
                 echo "Installing Flatpak" >> failed.txt
                 return 1
         fi
         if ! flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
             then
-                echo "Error during adding Flathub."
                 echo "Adding Flathub" >> failed.txt
                 return 1
         fi
@@ -270,7 +243,6 @@ function install_libre_office(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install libreoffice -y
             then
-                echo "Error during installing Libre Office."
                 echo "Installing Libre Office" >> failed.txt
                 return 1
         fi
@@ -282,7 +254,6 @@ function install_obsidian(){
     if [[ "$system" == "Fedora" ]]; then
         if ! flatpak install flathub md.obsidian.Obsidian -y
             then
-                echo "Error during installing Obsidian."
                 echo "Installing Obsidian" >> failed.txt
                 return 1
         fi
@@ -294,7 +265,6 @@ function install_modrinth(){
     if [[ "$system" == "Fedora" ]]; then
         if ! flatpak install flathub com.modrinth.ModrinthApp -y
             then
-                echo "Error during installing Modrinth."
                 echo "Installing Modrinth" >> failed.txt
                 return 1
         fi
@@ -306,7 +276,6 @@ function install_retroarch(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install retroarch -y
             then
-                echo "Error during installing Retroarch."
                 echo "Installing Retroarch" >> failed.txt
                 return 1
         fi
@@ -318,7 +287,6 @@ function install_keepassxc(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install keepassxc -y
             then
-                echo "Error during installing KeepassXC."
                 echo "Installing KeepassXC" >> failed.txt
                 return 1
         fi
@@ -330,7 +298,6 @@ function install_transmission(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install transmission -y
             then
-                echo "Error during installing transmission."
                 echo "Installing Transmission" >> failed.txt
                 return 1
         fi
@@ -342,7 +309,6 @@ function install_shotcut(){
     if [[ "$system" == "Fedora" ]]; then
         if ! flatpak install flathub org.shotcut.Shotcut -y
             then
-                echo "Error during installing Shotcut."
                 echo "Installing Shotcut" >> failed.txt
                 return 1
         fi
@@ -354,7 +320,6 @@ function install_neofetch(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install neofetch -y
             then
-                echo "Error during installing Neofetch."
                 echo "Installing Neofetch" >> failed.txt
                 return 1
         fi
@@ -366,7 +331,6 @@ function install_openrgb(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install openrgb -y
             then
-                echo "Error during installing OpenRGB."
                 echo "Installing OpenRGB" >> failed.txt
                 return 1
         fi
@@ -378,7 +342,6 @@ function install_mangohud(){
     if [[ "$system" == "Fedora" ]]; then
         if ! sudo dnf install mangohud -y
             then
-                echo "Error during installing MangoHud."
                 echo "Installing MangoHud" >> failed.txt
                 return 1
         fi
@@ -386,6 +349,23 @@ function install_mangohud(){
     fi
 }
 
+function update_system(){
+    if [[ "$system" == "Fedora" ]]; then
+        if ! sudo dnf update -y
+            then
+                echo "Updating system" >> failed.txt
+                return 1
+        fi
+        if ! sudo dnf upgrade -y
+            then
+                echo "Upgrading system" >> failed.txt
+                return 1
+        fi
+        echo "System updated!"
+    fi
+}
+
+update_system
 install_steam
 install_codecs
 install_git
@@ -413,3 +393,4 @@ install_mangohud
 echo "Installation complete!"
 echo "Failed installations:"
 echo cat failed.txt
+rm failed.txt
